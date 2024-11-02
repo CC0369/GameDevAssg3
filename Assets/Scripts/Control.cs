995e8d168b34a8d4f0b43ed2c5cc7edd1d6eb4e1
CC0369 <98926546+CC0369@users.noreply.github.com>
2024-11-03 00:32:46 +1100

    Minor changes, and added exitbutton

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -1,18 +1,61 @@
+using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class ButtonLoader : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
-    public Button levelOneButton;
-
-    void Start()
+    void Awake()
     {
-        levelOneButton.onClick.AddListener(LoadGameScene);
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        GameObject levelOneButton = GameObject.FindWithTag("LevelOne");
+        Button buttonComponent = levelOneButton?.GetComponent<Button>();
+        buttonComponent?.onClick.AddListener(LoadFirstLevel);
     }
 
-    void LoadGameScene()
+    public void LoadFirstLevel()
     {
-        SceneManager.LoadScene("GameScene");
+        StartCoroutine(LoadGameScene("GameScene"));
+    }
+
+    private IEnumerator LoadGameScene(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void ReturnToStartScreen()
+    {
+        StartCoroutine(LoadStartScene("StartScene"));
+    }
+
+    private IEnumerator LoadStartScene(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "GameScene")
+        {
+            GameObject exitButton = GameObject.FindWithTag("ExitButton");
+            Button buttonComponent = exitButton?.GetComponent<Button>();
+            buttonComponent?.onClick.AddListener(ReturnToStartScreen);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
